bee35c38f8e711d507438cfc2ff5ac5fa4a38940
Reverad <sektumpro@gmail.com>
2026-03-03 13:05:14 +0200

    update

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 
 class Animal:
-    alive = []
+    alive: list[Animal] = []
 
     def __init__(
             self,
@@ -17,8 +17,7 @@ class Animal:
 
     def __repr__(self) -> str:
         return (
-            f"{{"
-            f"Name: {self.name}, "
+            f"{{Name: {self.name}, "
             f"Health: {self.health}, "
             f"Hidden: {self.hidden}}}"
         )
